712ea8ded8b9196da5494326f406331343838bb3
Jolkas <ivalexsay@gmail.com>
2022-04-06 20:00:24 +0300

    Try to realize databinding

--- app/src/main/java/ivakin/first/my_first_app/MainActivity.kt
+++ app/src/main/java/ivakin/first/my_first_app/MainActivity.kt
@@ -2,6 +2,7 @@ package ivakin.first.my_first_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ivakin/first/my_first_app/TimeSettings.kt
+++ app/src/main/java/ivakin/first/my_first_app/TimeSettings.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ivakin.first.my_first_app.databinding.FragmentTimeSettingsBinding
+import ivakin.first.my_first_app.viewmodels.TimeSettingsViewModel
 
 class TimeSettings : Fragment() {
-
+    //private lateinit var viewModel: TimeSettingsViewModel
+    private lateinit var binding: FragmentTimeSettingsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_time_settings, container, false)
+    ): View {
+        binding = FragmentTimeSettingsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewmodel = TimeSettingsViewModel()
+        return binding.root
     }
 
 }
