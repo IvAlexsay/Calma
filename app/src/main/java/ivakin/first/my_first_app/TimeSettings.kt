89ebbffac9f7fa78b6788be3519233ccc4889316
Jolkas <ivalexsay@gmail.com>
2022-04-07 10:05:45 +0300

    Realize viewbinding

--- app/src/main/java/ivakin/first/my_first_app/TimeSettings.kt
+++ app/src/main/java/ivakin/first/my_first_app/TimeSettings.kt
@@ -5,23 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import ivakin.first.my_first_app.databinding.FragmentTimeSettingsBinding
+
 import ivakin.first.my_first_app.viewmodels.TimeSettingsViewModel
 
-class TimeSettings : Fragment() {
-    //private lateinit var viewModel: TimeSettingsViewModel
-    private lateinit var binding: FragmentTimeSettingsBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentTimeSettingsBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner = this
-        binding.viewmodel = TimeSettingsViewModel()
-        return binding.root
+class TimeSettings : Fragment(R.layout.fragment_time_settings) {
+    private var fragmentTimeSettingBinding: FragmentTimeSettingsBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentTimeSettingsBinding.bind(view)
+        fragmentTimeSettingBinding = binding
+        binding.textView.text = "fuck"
     }
 
+    override fun onDestroyView() {
+        fragmentTimeSettingBinding = null
+        super.onDestroyView()
+    }
 }
